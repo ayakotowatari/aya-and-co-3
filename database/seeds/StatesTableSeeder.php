a9438787f0b50db36f317aa422a38110aeacaeba
Ayako Towatari <kyotayako@gmail.com>
2022-01-24 10:44:37 +0900

    States table seeder

---
+++ database/seeds/StatesTableSeeder.php
@@ -0,0 +1,302 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class StatesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $data = [
+            [
+                "name" => "北海道",
+                "name_en" => "HOKKAIDO",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "青森県",
+                "name_en" => "AOMORI",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "岩手県",
+                "name_en" => "IWATE",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "宮城県",
+                "name_en" => "MIYAGI",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "秋田県",
+                "name_en" => "AKITA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+                
+            [
+                "name" => "山形県",
+                "name_en" => "YAMAGATA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "福島県",
+                "name_en" => "FUKUSHIMA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "茨城県",
+                "name_en" => "IBARAKI",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "栃木県",
+                "name_en" => "TOCHIGI",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "群馬県",
+                "name_en" => "GUMMA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "埼玉県",
+                "name_en" => "SAITAMA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "千葉県",
+                "name_en" => "CHIBA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "東京都",
+                "name_en" => "TOKYO",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "神奈川県",
+                "name_en" => "KANAGAWA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "新潟県",
+                "name_en" => "NIIGATA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "富山県",
+                "name_en" => "TOYAMA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "石川県",
+                "name_en" => "ISHIKAWA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "福井県",
+                "name_en" => "FUKUI",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "山梨県",
+                "name_en" => "YAMANASHI",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "長野県",
+                "name_en" => "NAGANO",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "岐阜県",
+                "name_en" => "GIFU",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "静岡県",
+                "name_en" => "SHIZUOKA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "愛知県",
+                "name_en" => "AICHI",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "三重県",
+                "name_en" => "MIE",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "滋賀県",
+                "name_en" => "SHIGA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "京都府",
+                "name_en" => "KYOTO",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "大阪府",
+                "name_en" => "OSAKA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "兵庫県",
+                "name_en" => "HYOGO",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "奈良県",
+                "name_en" => "NARA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "和歌山県",
+                "name_en" => "WAKAYAMA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "鳥取県",
+                "name_en" => "TOTTORI",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "島根県",
+                "name_en" => "SHIMANE",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "岡山県",
+                "name_en" => "OKAYAMA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "広島県",
+                "name_en" => "HIROSHIMA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "山口県",
+                "name_en" => "YAMAGUCHI",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "徳島県",
+                "name_en" => "TOKUSHIMA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "香川県",
+                "name_en" => "KAGAWA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "愛媛県",
+                "name_en" => "EHIME",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "高知県",
+                "name_en" => "KOCHI",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "福岡県",
+                "name_en" => "FUKUOKA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "佐賀県",
+                "name_en" => "SAGA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "長崎県",
+                "name_en" => "NAGASAKI",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "熊本県",
+                "name_en" => "KUMAMOTO",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "大分県",
+                "name_en" => "OITA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "宮崎県",
+                "name_en" => "MIYAZAKI",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "鹿児島県",
+                "name_en" => "KAGOSHIMA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "沖縄県",
+                "name_en" => "OKINAWA",
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+        ];
+                DB::table('states')->insert($data);
+    }
+}
